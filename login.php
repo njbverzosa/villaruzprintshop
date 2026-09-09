aa4c6877d747f5b431ff54d6e0cbf1b4ca8db78f
Nj Verzosa <njbverzosa@24>
2026-09-09 15:37:03 +0800

    push

--- login.php
+++ login.php
@@ -860,7 +860,7 @@ if (isset($_SESSION['exit_message'])) {
             <div class="download-section">
                 <span>
                     Download our app:
-                    <a href="http://villaruz-print-shop-and-general-merchandise.shop/Apk/app.apk" target="_blank">
+                    <a href="http://villaruz-print-shop-and-general-merchandise.shop/app.apk">
                         Download APP
                     </a>
                 </span>
