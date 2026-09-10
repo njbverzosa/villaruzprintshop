2e5bfc14bc9bcf9b37250d101ad67ceda7c0f57b
Nj Verzosa <njbverzosa@24>
2026-09-10 11:44:45 +0800

    push

--- login.php
+++ login.php
@@ -1044,7 +1044,7 @@ if (isset($_SESSION['exit_message'])) {
         // ==========================================
         biometricLoginBtn.addEventListener('click', function () {
             if (!isInApp) {
-                showBiometricStatus('Biometric login is only available in the app', 'info');
+                showBiometricStatus('Login with Device Security is only available in the app', 'info');
                 return;
             }
 
