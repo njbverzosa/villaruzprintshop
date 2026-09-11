6d29d981883335c3a047cef88711e154deb2cdae
Nj Verzosa <njbverzosa@24>
2026-09-11 11:45:03 +0800

    push

--- update_version.php
+++ update_version.php
@@ -1,6 +1,6 @@
 <?php
-$currentVersion = "1.0.0"; // (Old Version) Update this when you release a new version
-$latestVersion = "2.0.0"; // (New Version) Update this when you release a new version
+$currentVersion = "15.05.31"; // (Old Version) Update this when you release a new version
+$latestVersion = "20.05.32"; // (New Version) Update this when you release a new version
 
 // $currentVersion = "15.05.31";  // Was $latestVersion
 // $latestVersion = "20.05.32";   // New version
