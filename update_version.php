e25083cf4f4aac99a2c82506d1ebbf0dbd4bc8e1
Nj Verzosa <njbverzosa@24>
2026-09-12 12:46:45 +0800

    push

--- update_version.php
+++ update_version.php
@@ -1,6 +1,6 @@
 <?php
-$latestVersion = '22.18.48';   // ✅ MUST MATCH build.gradle versionName
-$currentVersion = '15.05.31';       // or whatever old version you had
+$latestVersion = '21.38.11';   // ✅ MUST MATCH build.gradle versionName
+$currentVersion = '21.18.48';       // or whatever old version you had
 
 // $currentVersion = "15.05.31";  // Was $latestVersion
 // $latestVersion = "20.10.32";   // New version
