7ca58e7de34918df996f0c0309f60159dce3e816
Nj Verzosa <njbverzosa@24>
2026-09-12 13:39:49 +0800

    push

--- login.php
+++ login.php
@@ -1143,9 +1143,7 @@ if (isset($_SESSION['exit_message'])) {
                         <img src="logo/ic_launcher.png" alt="Sofia App Logo">
                     </div>
                     <div class="update-title">SofiaApp</div>
-                    <div class="update-installed-version" id="installedVersionBadge">
-                        Detecting installed version...
-                    </div>
+                    
                     <div class="update-recommended">
                         Mobile browser access is no longer available. Please download the SofiaApp.
                     </div>
@@ -1173,6 +1171,10 @@ if (isset($_SESSION['exit_message'])) {
                         <li>
                             <span class="label">New Release</span>
                             <span class="value"><?php echo htmlspecialchars($latestVersion); ?></span>
+                        </li>
+                        <li>
+                            <span class="label">Current Version</span>
+                            <span id="installedVersionBadge" class="value"></span>
                         </li>
                         <li>
                             <span class="label">Old Version</span>
