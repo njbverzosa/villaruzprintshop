ad6e60d1af799ae4dac76956b2b37c63569d75de
Nj Verzosa <njbverzosa@24>
2026-09-16 13:27:14 +0800

    push

--- web/all_products.php
+++ web/all_products.php
@@ -990,8 +990,8 @@ foreach ($allProducts as $product) {
                         <i class="fas fa-times"></i> Clear
                     </button>
                 </div>
-                <a href="download_images.php" class="add-product-btn" id="addProductBtn">
-                    <i class="fas fa-plus-circle"></i> Download
+                <a href="upload_products.php" class="add-product-btn" id="addProductBtn">
+                    <i class="fas fa-plus-circle"></i> Add New
                 </a>
 
                 <?php if ((int) ($user['authorize_access'] ?? -1) === 0): ?>
