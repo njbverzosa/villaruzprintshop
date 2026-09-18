5d8858074476955aa9fbd735f776edd73ee6b98a
Nj Verzosa <njbverzosa@24>
2026-09-18 10:56:18 +0800

    push

--- billing_receipt.php
+++ billing_receipt.php
@@ -525,9 +525,9 @@ if (!empty($qrData)) {
     <div class="bill-paper">
         
         <div class="shop-header">
-            <div class="shop-name">VILLARUZ PRINTSHOP & GENERAL MERCHANDISE</div>
-            <div class="shop-address">Poblacion 2411, Dasol, Pangasinan, Philippines</div>
-            <div class="vat-row">VAT Reg. TIN: 257-630-627-00000</div>
+            <div class="shop-name">SOPHIELISETAKATED OFFICE AND SCHOOL SUPPLIES</div>
+            <div class="shop-address">Gais-Guipe Dasol Pangasinan</div>
+            <div class="vat-row">VAT Reg. TIN: 487-060-298-00000</div>
         </div>
 
         <div class="doc-title">
